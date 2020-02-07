b9198b6d5a5e44d2e57d43e2a7770ec6248b84e6
adam <adam.nawroc@gmail.com>
2020-02-06 21:52:04 -0500

    hitting api from frontend

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <p>hi world</p>
-    </div>
-  );
+class App extends Component {
+  state = {
+    chain: []
+  }
+
+  componentDidMount() {
+    axios
+    .get('http://localhost:5000/chain')
+    .then(response => this.setState({chain: response.data}))
+    .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>Current length of chain: {this.state.chain.length}</p>
+      </div>
+    );    
+  }
 }
 
 export default App;
